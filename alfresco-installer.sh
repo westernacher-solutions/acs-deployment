c4e7975a110c4bb71eb49055c29727a65e144f1d
Torsten Werner <torsten.werner@westernacher.com>
2019-06-18 10:48:41 +0200

    disable nginx-ingress in values.yaml

--- alfresco-installer.sh
+++ alfresco-installer.sh
@@ -5,5 +5,5 @@ PATH=$PATH:/
 tiller &
 sleep 1
 cd helm
-HELM_HOST=localhost:44134 helm install alfresco-content-services --name demo --set alfresco-infrastructure.nginx-ingress.enabled=false "$@"
+HELM_HOST=localhost:44134 helm install alfresco-content-services --name demo "$@"
 killall tiller
